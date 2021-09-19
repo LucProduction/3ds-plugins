cc7e14da94c76665ac45a7c968971d8a53cc1630
Lukasz Tomczewski <lukasz.tomczewski@cgtrader.com>
2021-09-19 18:23:20 +0200

    Define plugin destination

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Script to generate Sierpinski Triangle as a plugin i 3dsMax
 
-
+# new line
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
